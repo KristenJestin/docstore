ded6afcda0b1ed80d55145641bb9e5aa27c8c257
kris <kristen.jestin@pm.me>
2026-09-07 20:46:16 +0200

    fix(web): move toasts back to the bottom right, above sheet footers and devtools
    (cherry picked from commit c3a13f8c07866e38f3cfdf2fa024b3a48fbb637b)

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -89,7 +89,7 @@ function RootDocument() {
 					<Outlet />
 					{/* Top right: bottom-right toasts covered the sheet and dialog
 					    footers, i.e. the very buttons the next action needs. */}
-					<Toaster position="top-right" />
+					<Toaster position="bottom-right" offset={{ bottom: 88, right: 16 }} />
 				</ThemeProvider>
 				<Devtools />
 				<Scripts />
